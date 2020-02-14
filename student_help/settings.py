79ed1e82b2a1a53b2e5a818b7b669b9dd6f4b5db
Dev Raj <rehansingh.4522@gmail.com>
2020-02-14 17:19:36 +0530

    Collectstatic is enabled

--- student_help/settings.py
+++ student_help/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '54rnamdr@x$uh-w+9h(u#q=8tg@(ghp97z3o4p92g6oeuou6t%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["theyuva.herokuapp.com"]
+ALLOWED_HOSTS = ['theyuva.heroku.com']
 
 
 # Application definition
