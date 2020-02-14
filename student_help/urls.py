a759c7b04eddbd29d5a39f21fa0c9f1812cbdc42
Dev Raj <rehansingh.4522@gmail.com>
2020-02-14 18:22:22 +0530

    there is error

--- student_help/urls.py
+++ student_help/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.sitemaps.views import sitemap
+from job_openings.Postsitemaps import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('job_openings.urls', namespace='job')),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, )
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
